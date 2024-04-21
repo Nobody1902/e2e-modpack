b009212749d1ead7148a1fcfdcbdc5420fbec73b
Nobody1902 <vitan.s.jaklic@gmail.com>
2024-04-21 15:26:49 +0200

    Added minecraft version to version.txt

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ import os
 version_txt = os.path.join(os.curdir, "version.txt")
 
 version = 1.0
+minecraft_version = "1.12.2-forge-14.23.5.2860"
 
 data = {
-    "version": version
+    "version": version,
+    "minecraft_version": minecraft_version
 }
 
 with open(version_txt, mode="w") as file:
